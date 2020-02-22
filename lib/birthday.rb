219631c1fc70a03662b3ab42c36a86c400761c47
Melissa Sperry <sperry.mel@gmail.com>
2020-02-22 04:51:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,7 @@ def happy_birthday(birthday_kids)
 end
 end
 
-def age_appropriate_birthday
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
   puts "You are too old for this."
 end
